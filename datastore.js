7d1a131985f0c7a44881baf39573432616cd630b
Lydia Hendriks <hendriks.lydia@gmail.com>
2019-07-05 11:41:01 -0400

    added JS,  updated Q&A, refined CSS

--- datastore.js
+++ datastore.js
@@ -1,61 +1,62 @@
 const STORE = [
 {
   question: 'Before taking a boat out it is important to know:',
-  answers: ['Tide', 'Weather', 'Wind speed/direction', 'All of that stuff!'],
-  correctAnswer: 'All of that stuff!',
-  icon: 'https://www.iconsdb.com/icons/preview/white/view-details-xxl.png',
-  altText: 'list icon'
+  answers: ['Tide', 'Weather', 'Wind speed/direction', 'All of the above'],
+  correctAnswer: 'All of the above',
+  correctAnswerMsg: 'Always check the local, route, and destination weather and water information before going out.'
 },
 {
-  question: 'Under maritime law, it is unusual for the courts to find only one vessel totally at fault. If a collision occurred, it tends to support the theory that both vessels did not do their jobs well.',
-  answers: ['True', 'False', 'IDK', 'I like boats'],
-  correctAnswer: 'True',
-  icon: 'https://www.iconsdb.com/icons/preview/white/sail-boat-xxl.png',
-  altText: 'boat icon'
+  question: 'Passengers are safe to board/exit the boat from the water when:',
+  answers: ['The engine is idle', 'The engine is off', 'Anytime as long as the captain is wearing the engine cut-off switch lanyard', 'Anytime if they are exiting from the side or bow of the boat'],
+  correctAnswer: 'The engine is off',
+  correctAnswerMsg: 'Drivers should never allow passengers to board or exit the boat from the water when the engine is on or idling. Drivers should keep watch around the propeller area when people are in the water.',
 },
 {
   question: 'Proper anchoring at islands requires:',
-  answers: ['Two anchors to prevent wind and tide pushing the boat up onto the shore or swinging into other boats', 'One anchor - the line must be taught
-', 'A trolling motor to navigate the shallow water around an island
-', 'Just throw out the anchor, duh'],
-  correctAnswer: 'Two anchors to prevent wind and tide pushing the boat up onto the shore or swinging into other boats',
-  icon: 'https://www.iconsdb.com/icons/preview/white/palm-tree-2-xxl.png',
-  altText: 'island icon'
+  answers: ['Rolly anchorage', 'Bow and Stern anchorage', 'Bahamian Moor', 'Backing an anchor'],
+  correctAnswer: 'Bow and Stern anchorage',
+  correctAnswerMsg: 'Two anchors prevent wind and tide pushing the boat up onto the shore or swinging into other boats.',
 },
 {
   question: 'The pecking order of right of way between vessels is:',
   answers: ['Fishing boats → Sail boats → Power boats', 'Sail Boats → Fishing boats → Power boats', 'Power boats → Fishing boats → Sail boats', 'None of those'],
-  correctAnswer: 'Fishing boats → Sail boats → Power boats
-',
-  icon: 'https://www.iconsdb.com/icons/preview/white/stop-3-xxl.png',
-  altText: 'hand-stop-signal icon'
+  correctAnswer: 'Fishing boats → Sail boats → Power boats',
+  correctAnswerMsg: 'The most maneuverable vessel gives way. However, responsibility for avoiding collisions is shared by everyone using the waterway.',
 },
 {
   question: 'How far away should boaters stay from divers down warning devices in open water?',
   answers: ['50 feet', '300 feet', '1200 feet', '1800 feet'],
   correctAnswer: '300 feet',
-  icon: 'https://www.iconsdb.com/icons/preview/white/fish-xxl.png',
-  altText: 'fish icon'
+  correctAnswerMsg: 'Boaters must make reasonable efforts to stay at least 300 feet away from divers-down warning devices in open water and at least 100 feet away in rivers, inlets, or navigation channels. If boats get closer than that they must slow to idle speed.',
 },
 {
   question: 'What does a red flag with white diagonal stripe signify?',
   answers: ['U.S. Divers down flag', 'The boat requires a tug', 'Bird sanctuary area', 'Boat party'],
   correctAnswer: 'U.S. Divers down flag',
-  icon: 'https://www.iconsdb.com/icons/preview/white/flag-xxl.png',
-  altText: 'flag icon'
+  correctAnswerMsg: 'Scuba divers and snorkelers display a divers-down warning device whenever they are in the water. The symbol is a red rectangle or square with a white diagonal stripe.',
 },
 {
   question: 'Riding on the bow of a boat is:',
-  answers: ['Always safe, and really fun', 'Permitted at the boat driver’s discretion', 'Against the Code of Federal Regulations - the passenger could fall overboard and be run over', 'Permitted depending on the boat type/speed'],
-  correctAnswer: 'Against the Code of Federal Regulations - the passenger could fall overboard and be run over',
-  icon: 'https://www.iconsdb.com/icons/preview/white/sun-2-xxl.png',
-  altText: 'sun icon'
+  answers: ['Always safe, and really fun', 'Permitted at the boat driver’s discretion', 'Against the Code of Federal Regulations', 'Permitted depending on the boat type/speed'],
+  correctAnswer: 'Against the Code of Federal Regulations',
+  correctAnswerMsg: 'The passenger could fall overboard and be run over. In some states bow riding is considered reckless and negligent operation.',
 },
 {
   question: 'The distress and safety call channel is:',
   answers: ['Channel 1', 'Channel 911', 'Channel 16', 'Different for each area'],
   correctAnswer: 'Channel 16',
-  icon: 'hhttps://www.iconsdb.com/icons/preview/white/marine-radio-xxl.png',
-  altText: 'marine radio icon'
+  correctAnswerMsg: 'Channel 16 VHF is a marine VHF radio frequency designated as an international distress frequency primarily intended for distress, urgency and safety priority calls.',
+},
+{
+  question: 'Which of the following is NOT a minimum safety equipment requirement?',
+  answers: ['Whistle or horn', 'Fire extinguisher', 'A PFD for every person', 'Polarized sunglasses'],
+  correctAnswer: 'Polarized sunglasses',
+  correctAnswerMsg: 'The U.S. Coast Guard has compiled a list of required boating safety equipment. Drivers should be familiar with the complete list and ensure all items are accounted for.',
+},
+{
+  question: 'If you are caught boating without the necessary safety equipment required by law, you may be fined:',
+  answers: ['Up to $1,000 per citation', 'Individually for each item you are missing', 'Usually the fines will range $50-$125 depending on what/how many items you are missing (e.g. $50 per missing life jacket)', 'All of the above'],
+  correctAnswer: 'All of the above',
+  correctAnswerMsg: 'The US Cost Guard (USCGA) offers free Vessel Safety Checks as a safeguard for boaters to ensure they meet federal safety standards. Vessels passing the safety check are awarded a decal indicating that they are in compliance.',
 },
 ];
--- index.js
+++ index.js
@@ -1,46 +1,153 @@
 
+let questionNumber = 0;
+let score = 0 ;
 
-//---------render current question number---------------
-let questionNumber = 1;
-let score = 1;
-
-function updateQuestionNumber() {
+//---------listen for click to start quiz, then hide 'js-start-quiz' section on click & unhide 'js-quiz-form'-------------
+function startQuiz() {
   $('#js-q-number').text(questionNumber);
-}
-
-//---------render current Score-----------
-function updateScore() {
   $('#js-score').text(score);
-}
-
-//-------render stats--------
-function renderStats() {
-updateQuestionNumber();
-updateScore();
-}
-
-$(renderStats);
-
-
-
-//---------START QUIZ-------------------------
-//---------start Quiz: remove HTML-------------
-function removeStartElements() {
   $('.js-start-button').click(function(){
-  $('.js-start-quiz').remove();
-  $('.js-quiz-form').css('display','block');
-});
+    $('.js-start-quiz').remove();
+    $('.js-quiz-section').css('display','block');
+    $('#js-q-number').text(questionNumber+1);
+  });
 }
 
-//---------start Quiz: add new HTML-----------
-
-
-//------Call functions to START QUIZ
-function startQuiz(){
-  removeStartElements();
+//---------Render html for final results----------
+function renderResults() {
+   function resultMsg() {
+    if (score < 5) {return 'Yikes - You better stick to the beaches for now...'}
+      else if (score <8) {return 'Second mate status - not too shabby!'}
+        else {return 'Way to go, Captain! You know your stuff!'};}
+  $('.stats').remove();
+  $('.js-quiz-section').html(`
+    <div class="feedback">
+    <h1>Final Score:</h1>
+    <h1>${score}/10 Correct</h1>
+    <h2>${resultMsg()}</h2>
+    <button type="button" class="js-restart-button restart-button">Retake Quiz</button>
+    </div>`
+  );
+  }
+//--------listen for click on restart quiz button-------
+function restartQuiz() {
+  $('.js-quiz-section').on('click', '.js-restart-button', function(){
+    location.reload();
+  });
 }
 
-$(startQuiz);
+//---------generate html for quiz questions-----------
+function generateQuestion(){
+  console.log('generateQuestion called');
+  if (questionNumber < STORE.length) {
+    return `<h1>${STORE[questionNumber].question}</h1>
+          <form id="js-quiz-form">
+            <fieldset class="radio-group">
+              <label class="option row1">
+                <input type="radio" value="${STORE[questionNumber].answers[0]}" name="answer" required><span>${STORE[questionNumber].answers[0]}</span>
+              </label>
+              <label class="option row2">
+                <input type="radio" value="${STORE[questionNumber].answers[1]}" name="answer" required><span>${STORE[questionNumber].answers[1]}</span>
+              </label>
+              <label class="option row3">
+                <input type="radio" value="${STORE[questionNumber].answers[2]}" name="answer" required><span>${STORE[questionNumber].answers[2]}</span>
+              </label>
+              <label class="option row4">
+                <input type="radio" value="${STORE[questionNumber].answers[3]}" name="answer" required><span>${STORE[questionNumber].answers[3]}</span>
+              </label>
+              <button type="submit" class="submit-button">Submit</button>
+            </fieldset>
+            </form>`;
+  }
+    else {
+      renderResults();
+      restartQuiz();
+    }
+  }
+
+//Render to DOM html for quiz questions--------
+  function renderQuestion() {
+    $('.js-quiz-section').html(generateQuestion());
+  }
+
+  //--------handle user answer & go to next question------
+
+  function answeredCorrectly() {
+    score ++;
+    $('#js-score').text(score);
+    $('.js-quiz-section').html(`
+      <div class="feedback">
+        <img src="https://image.flaticon.com/icons/svg/609/609702.svg" alt="celebration icon">
+        <h2>Correct!</h2>
+        <p>${STORE[questionNumber].correctAnswerMsg}</p>
+        <button type="button" class="next-button js-next-button">Next</button>
+      </div>`
+    );
+  }
+
+  function answeredIncorrectly() {
+    $('.js-quiz-section').html(`
+      <div class="feedback">
+      <img src="https://image.flaticon.com/icons/svg/148/148766.svg" altText="incorrect \'x\' icon">
+      <h2>Nope, that wasn't it</h2>
+      <p>The correct answer is: "${STORE[questionNumber].correctAnswer}"</p>
+      <p>${STORE[questionNumber].correctAnswerMsg}</p>
+      <button type="button" class="next-button js-next-button">Next</button>
+      </div>`
+    );
+  }
+
+  function goToNextQuestion(){
+    $('.js-quiz-section').on('click', '.js-next-button', function(){
+      incrementQuestion();
+      generateQuestion();
+      renderQuestion();
+      $('#js-q-number').text(questionNumber+1)
+      handleUserAnswers();
+      checkedLabelColor();
+    });
+  }
+
+  function handleUserAnswers(){
+    $('form').submit(function(event) {
+    console.log('handleUserAnswers called');
+    event.preventDefault();
+    let selected = $('input:checked');
+    let answer = selected.val();
+    let correctAnswer = `${STORE[questionNumber].correctAnswer}`
+    if (answer === correctAnswer) {
+      selected.parent().addClass('correct');
+      answeredCorrectly();
+    } else {
+      selected.parent().addClass('wrong');
+      answeredIncorrectly();
+    }
+  });
+}
+
+  function incrementQuestion() {
+      questionNumber ++;
+    }
+
+  function updateScore() {
+    console.log('updateScore called');
+
+  }
+
+
+//------Call all the functions to take the quiz
+function takeQuiz(){
+  startQuiz();
+  renderQuestion();
+  handleUserAnswers();
+  goToNextQuestion();
+}
+
+$(takeQuiz);
+
+
+
+
 
 //jQuery for stylesheet
 
